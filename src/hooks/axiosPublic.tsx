ca60f4c5093f04624faacf3dfb5ea380711138e1
asuselessbrain <arfan18@cse.pstu.ac.bd>
2026-06-11 00:53:43 +0600

    fix: build error

--- src/hooks/axiosPublic.tsx
+++ src/hooks/axiosPublic.tsx
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-<<<<<<< HEAD
-    baseURL: "http://119.15.153.74/api/v1",
-=======
     baseURL: "http://119.15.153.74/api/api/v1",
->>>>>>> 3723ddb14f3751ad8a76c2ae126e5dc27b18a8c9
     withCredentials: true,
 });
 
