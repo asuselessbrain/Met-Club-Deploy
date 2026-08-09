d636032309b04dffd5379a778106bec7a0863060
asuselessbrain <arfan18@cse.pstu.ac.bd>
2026-08-09 23:07:30 +0600

    fix: link

--- src/pages/StartJourney.tsx
+++ src/pages/StartJourney.tsx
@@ -110,7 +110,7 @@ export default function StartJourney() {
               btnShadow="rgba(249,115,22,0.42)"
               btnLabel={isChapterOneCompleted ? "সিমুলেশনে প্রবেশ করুন" : "প্রথম অধ্যায় সম্পন্ন করুন"}
               // disabled={!isChapterOneCompleted}
-              link="/games"
+              link="/games/"
               description={
                 <>
                   <strong>আপনার দক্ষতা পরীক্ষা করুন!</strong>
